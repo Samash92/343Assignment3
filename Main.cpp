75df98b43274fc144252dc96075b46e08e3b6d44
Samash92 <tsampson2092@gmail.com>
2018-06-01 12:57:22 -0700

    Forgot to close the inputStream

--- Main.cpp
+++ Main.cpp
@@ -7,5 +7,6 @@ int main(int argc, char* argv[]) {
 	std::ifstream& inputStream("conduits.txt");
 	Travel_Times* conduitTimes = new Trave_Times(inputStream);
 	conduitTimes->dump();
+	inputStream.close();
 	delete conduitTimes;
 }
